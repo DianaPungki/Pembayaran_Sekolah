4035d8afa07082b7bddc5ff6c7198fef40ac7268
DianaPungky <dianapungki379@gmail.com>
2021-09-20 09:50:03 +0700

    menambahkan halaman dashboard

--- resources/views/admin/dashboard/dashboard.blade.php
+++ resources/views/admin/dashboard/dashboard.blade.php
@@ -1,43 +1,107 @@
 @extends('admin/master')
 @section('content')
-<div class="content-wrapper">
-  <div class="content-header">
-    <div class="container-fluid">
-      <div class="row mb-2">
-        <div class="col-sm-6">
-          <h1 class="m-0">Dashboard</h1>
+    <div class="content-wrapper">
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Dashboard</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item">Dashboard</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="col-sm-6">
-          <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item">Dashboard</li>
-          </ol>
-        </div>
-      </div>
+
+        <section class="content">
+            <div class="container-fluid">
+
+                {{-- <div class="card">
+                    <div class="card-header">
+                        Dashboard
+                    </div>
+                    <div class="card-body"> --}}
+
+                {{-- NULIS KONTEN VIEW DISINI --}}
+
+                <div class="row">
+                    <div class="col-lg-3 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-info">
+                            <div class="inner">
+                                <h3>150</h3>
+
+                                <p>Data Kelas</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-folder"></i>
+                            </div>
+                            <a href="{{ url('admin/modulsiswa/kelas') }}" class="small-box-footer">More info <i
+                                    class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                    <!-- ./col -->
+                    <div class="col-lg-3 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-success">
+                            <div class="inner">
+                                <h3>53</h3>
+
+                                <p>Data Siswa</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-person-stalker"></i>
+                            </div>
+                            <a href="{{ url('admin/modulsiswa/siswa') }}" class="small-box-footer">More info <i
+                                    class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                    <!-- ./col -->
+                    <div class="col-lg-3 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-warning">
+                            <div class="inner">
+                                <h3>44</h3>
+
+                                <p>Data Pembayaran</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-clipboard"></i>
+                            </div>
+                            <a href="{{ url('admin/modultagihan/pembayaran') }}" class="small-box-footer">More info <i
+                                    class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                    <!-- ./col -->
+                    <div class="col-lg-3 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-danger">
+                            <div class="inner">
+                                <h3>65</h3>
+
+                                <p>Data Tagihan</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-pie-graph"></i>
+                            </div>
+                            <a href="{{ url('admin/modultagihan/tagihan') }}" class="small-box-footer">More info <i
+                                    class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                    <!-- ./col -->
+                </div>
+
+                {{-- </div>
+                </div> --}}
+            </div>
+        </section>
     </div>
-  </div>
 
-  <section class="content">
-    <div class="container-fluid">
-
-      <div class="card">
-        <div class="card-header">
-            Dashboard
-        </div>
-        <div class="card-body">
-            
-            {{-- NULIS KONTEN VIEW DISINI --}}
-
-        </div>
-    </div>
-
-    </div>
-  </section>
-</div>
-
-<script>
-
-    // Nulis Kode JS disini
-
-</script>
+    <script>
+        // Nulis Kode JS disini
+    </script>
 
 @endsection
